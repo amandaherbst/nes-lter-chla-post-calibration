16ad5a0965fe4b332fdd70df051fdfda4160c88b
amandaherbst <amandaherbst@201.38.20.172.s.wireless.uri.edu>
2021-08-05 13:57:19 -0400

    creates discrete underway output file

--- matlab_code/match_uw_discrete_continuous.m
+++ matlab_code/match_uw_discrete_continuous.m
@@ -30,7 +30,7 @@ for i1=1:length(cruise)
     iso8601format = 'yyyy-mm-dd hh:MM:ss';
     Time_UW = datenum(table_uw.date, iso8601format);
     
-    %Get the Latitude, Longitude, Fluo1/Fluo2 in Volt and in ug/L from the table
+    %Get the Latitude, Longitude, Fluo1/Fluo2 in ug/L from the table
     Latitude=table_uw.gps_furuno_latitude;
     Longitude=table_uw.gps_furuno_longitude;
     Fluo1_wetstar=table_uw.tsg1_fluorescence_wetstar;
@@ -53,8 +53,7 @@ for i1=1:length(cruise)
         'replicate','filter_size','chl','phaeo','iode_quality_flag',...
         'fluorescence_wetstar_match','fluorescence_ecofl_match'});
     
-    %Find for each discrete data the corresponding lat/lon (exact time),
-    %the fluorescence (exact times) 
+    %Find for each discrete data the corresponding lat/lon & fluorescence  
     for i2=1:length(Time_discrete)
         
         %Find the minimal difference between the sampling time and the
